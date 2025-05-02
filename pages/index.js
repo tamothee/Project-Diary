9dc8dfea06bd7d2b12b849647906930a0fbc5979
tamothee <timo9132@gmail.com>
2025-05-02 21:58:06 +0800

    spacing between logos

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
             </p>
             <div className="display: inline">
               <Link href="https://github.com/tamothee" passHref={true}>
-              <a className="b">
+              <a className="b pr-4">
                 <Image src={"/GithubLogo.png"} alt="" width={40} height={40} />
               </a>
             </Link>
